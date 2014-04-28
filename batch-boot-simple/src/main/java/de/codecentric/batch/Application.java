bf266fe241b6401169f07b8cfc4d559f312d52f7
Tobias Flohre <tobias.flohre@codecentric.de>
2014-04-28 22:01:20 +0200

    Example for using own BatchConfigurer

--- batch-boot-simple/src/main/java/de/codecentric/batch/Application.java
+++ batch-boot-simple/src/main/java/de/codecentric/batch/Application.java
@@ -11,4 +11,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    // If you would like to configure your own batch infrastructure via BatchConfigurer,
+    // just add a bean of that type to the ApplicationContext, like in the following code.
+    // This starter's implementation will step aside then.
+//    @Bean
+//    public BatchConfigurer batchConfigurer(DataSource dataSource){
+//    	return new DefaultBatchConfigurer(dataSource);
+//    }
 }
