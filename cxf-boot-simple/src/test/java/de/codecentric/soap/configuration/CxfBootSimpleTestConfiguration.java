2bf01b8d2f29abb30a81262fd0f51afc53538af3
Jonas Hecht <jonas.hecht@codecentric.de>
2019-04-15 12:48:22 +0200

    Activate SoapMessageLogging in client mode also

--- cxf-boot-simple/src/test/java/de/codecentric/soap/configuration/CxfBootSimpleTestConfiguration.java
+++ cxf-boot-simple/src/test/java/de/codecentric/soap/configuration/CxfBootSimpleTestConfiguration.java
@@ -1,5 +1,8 @@
 package de.codecentric.soap.configuration;
 
+import de.codecentric.cxf.logging.soapmsg.SoapMessageLoggingInInterceptor;
+import de.codecentric.cxf.logging.soapmsg.SoapMessageLoggingOutInterceptor;
+import org.apache.cxf.interceptor.AbstractLoggingInterceptor;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,7 +40,25 @@ public class CxfBootSimpleTestConfiguration {
         JaxWsProxyFactoryBean jaxWsFactory = new JaxWsProxyFactoryBean();
         jaxWsFactory.setServiceClass(WeatherService.class);
         jaxWsFactory.setAddress(buildUrl());
+        jaxWsFactory.getInInterceptors().add(logInInterceptorClientSoapMsgLogger());
+        jaxWsFactory.getOutInterceptors().add(logOutInterceptorClientSoapMsgLogger());
         return (WeatherService) jaxWsFactory.create();
+    }
+
+    @Bean
+    public AbstractLoggingInterceptor logInInterceptorClientSoapMsgLogger() {
+        SoapMessageLoggingInInterceptor logInInterceptor = new SoapMessageLoggingInInterceptor();
+        logInInterceptor.logSoapMessage(true);
+        logInInterceptor.setPrettyLogging(true);
+        return logInInterceptor;
+    }
+
+    @Bean
+    public AbstractLoggingInterceptor logOutInterceptorClientSoapMsgLogger() {
+        SoapMessageLoggingOutInterceptor logOutInterceptor = new SoapMessageLoggingOutInterceptor();
+        logOutInterceptor.logSoapMessage(true);
+        logOutInterceptor.setPrettyLogging(true);
+        return logOutInterceptor;
     }
     
     /*
